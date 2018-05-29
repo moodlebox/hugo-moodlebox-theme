fb2f90376ff090ad1e2cc744db2064c95e2a0588
Nicolas Martignoni <nicolas@martignoni.net>
2018-05-29 22:14:21 +0200

    Using now a base template. Issue #2. See https://gohugo.io/templates/base/.

--- static/js/moodlebox.js
+++ static/js/moodlebox.js
@@ -1,5 +1,6 @@
 /* MoodleBox web site specific Javascript code */
 jQuery(document).ready(function (e) {
+
   /* Make dropdown link work correctly when clicked */
   $('.dropdown-toggle').click(function (e) {
     if ($(document).width() > 750) {
@@ -22,43 +23,6 @@ jQuery(document).ready(function (e) {
       $(this).children('a').blur()
       $(this).children('a').removeClass('open')
     }
-  })
-
-  /* Contact form */
-  $(function () {
-    $('#contact').find('input,select,textarea').jqBootstrapValidation({
-      preventSubmit: true,
-      submitError: function ($form, event, errors) {
-      },
-      submitSuccess: function ($form, e) {
-        e.preventDefault()
-        var submitButton = $('input[type=submit]', $form)
-        $.ajax({
-          type: 'POST',
-          // Set real action URL
-          url: 'https://hooks.zapier.com/hooks/catch/2849143/8rav6j/',
-          data: $form.serialize(),
-          beforeSend: function (xhr, opts) {
-            if ($('#_email', $form).val()) {
-              xhr.abort()
-            } else {
-              submitButton.prop('value', 'Please Wait...')
-              submitButton.prop('disabled', 'disabled')
-            }
-          }
-        }).done(function (data) {
-          submitButton.prop('value', 'Thank you, we\x27ll get back to you shortly.')
-          submitButton.prop('disabled', false)
-        })
-      },
-      filter: function () {
-        return $(this).is(':visible')
-      }
-    })
-  })
-
-  $('#name').focus(function () {
-    $('#success').html('')
   })
 
   /* Donation form */
@@ -99,4 +63,5 @@ jQuery(document).ready(function (e) {
       }
     })
   })
+
 })
